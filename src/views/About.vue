99eafd925be0dcec99355fa4dfd831ccfa7a0930
esteloabell <manitobaaa@gamil.com>
2020-11-28 19:52:46 -0600

    initial commit, first vue repository

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about-card">
+    <div class="card-title">
+      <img src="https://avatars2.githubusercontent.com/u/49965889?s=460&u=a33bdcda0c260bbdd896071495a26ef72e212351&v=4" alt="avatar" />
+    </div>
+  </div>
+</template>
